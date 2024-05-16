8533e652f1bb8ff95c09ecf3e2c6b16afbadefd7
Ravi Verma <111685794+usoolhaimera@users.noreply.github.com>
2024-05-16 23:39:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// --------------- sidemenu ---------------
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemeu.style.right = "0";
+}
+function closemenu() {
+  sidemeu.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxdIp1cMMNaunI9XesH25cDYPo9kFOAxkKmhZNnmDxPiUCe4WpDI2ic44jjdW9kDo4LJA/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 4000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
